2a4307ee1a8140c307f3c46f90a866d970e17fca
Muna Bhattarai <76533492+GitHer-Muna@users.noreply.github.com>
2024-06-05 17:02:06 +0000

    Modified: src/App.jsx Modified: src/Components/About.jsx Modified: src/Components/Home.jsx Modified: src/Components/Portfolio.jsx Added: src/images/about.jpg Deleted: src/images/design-desk.jpeg Added: src/images/home.jpg Deleted: src/images/motion-background.jpg Added: src/images/portfolio.jpg

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/about.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "laptop and coffee on a table";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an alpha Microsoft Learn Student Ambassador. I lead technical on-campus community.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Organizational",
+  "Management",
+  "Leadership",
+  "Team player",
+  "Public speaking",
+  "Mentorship",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about making knowledge around technology accessible to everyone. I believe that technology is a tool that can be used to solve some of the world's biggest problems. I am a strong believer in the power of community and the impact that it can have on the world. I am a strong advocate for diversity and inclusion in the tech industry.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desktop with colorful pencil leads on left";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "The Future of Serverless Computing 🚀",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Insights on the latest trends and advancements in serverless computing.",
+    url: "https://examples.com",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Revolutionizing Web Development",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Exploring the transformative power of modern web development tools.",
+    url: "https://examples.com",
   },
   {
-    title: "My Resume Site",
+    title: "Crafting Digital Identities",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A journey through crafting engaging online portfolios and resumes.",
+    url: "https://examples.com",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "The Evolution of Developer Tools",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A comprehensive discussion on the latest advancements in developer tools.",
+    url: "https://examples.com",
   },
 ];
 
